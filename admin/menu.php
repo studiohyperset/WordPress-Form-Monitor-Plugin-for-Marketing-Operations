7c1a99ad45dcd137434882163129bd76b1a6d40c
Bruno Cantuaria <bruno@cantuaria.net.br>
2016-07-06 22:48:04 -0300

    mpdf

--- admin/menu.php
+++ admin/menu.php
@@ -143,3 +143,16 @@ function form_monitor_plugin_page(  ) {
 	<?php
 
 }
+
+
+add_action( 'admin_init', 'infer_form_generate_pdf' );
+function infer_form_generate_pdf() {
+	if (isset($_GET['page']) && $_GET['page']=='form_monitor') {
+		if (isset($_GET['generate']) && $_GET['generate']=='pdf') {
+			include_once('pdf/pdf.php');
+			die();
+		}
+	}
+
+	return;
+}
